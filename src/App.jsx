a7a19f772c1c811b3fce3fbda63fe6f49a93c2b5
CodeSandbox <support@codesandbox.io>
2023-01-27 16:18:35 +0000

    "Initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import "./App.css";
+
+export function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="p-8 bg-white shadow-md rounded-md">
+        <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
+        <p className="text-lg">Welcome to De Diversity Dimension</p>
+      </div>
+    </div>
+  );
+}

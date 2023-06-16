a9ef72e1156c3100fcce65c12483cd54a78aef42
2922_VAISHNAVI KALE <vaishnavi.a.kale@cumminscollege.in>
2023-06-16 20:26:46 +0000

    initialization

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ export function App() {
       <div className="p-8 bg-white shadow-md rounded-md">
         <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
         <p className="text-lg">Welcome to De Diversity Dimension</p>
+        <p class="font-sans md:font-serif">Vaishnavi Kale</p>
+        <div class="text-5xl font-extrabold ...">
+          <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            De Diversity Dimension
+          </span>
+        </div>
       </div>
     </div>
   );
